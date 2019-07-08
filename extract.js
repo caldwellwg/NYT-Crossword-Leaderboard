35a27c18a7cee2f4c0ce3706f9e94aef33900134
caldwellwg <caldwellwg@gmail.com>
2019-07-08 00:20:21 -0500

    Fixed a date bug and added date to JSON.

--- extract.js
+++ extract.js
@@ -7,13 +7,20 @@
 // Identify the bar div to add the new button
 buttonBar = document.getElementsByClassName("lbd-board__actions")[0]
 
+// Get a date string
+d = new Date()
+dateString = [d.getFullYear(), d.getMonth() + 1, d.getDate()].join('-') 
+
 // Create the blob URL for the JSON
-json = Array.from(document.getElementsByClassName("lbd-score")).map(item =>
-    ({
-        name: item.childNodes[1].childNodes[0].data,
-        time: item.childNodes[2].childNodes[0].data
+json = ({
+        data: Array.from(document.getElementsByClassName("lbd-score")).map(item =>
+            ({
+                name: item.childNodes[1].childNodes[0].data,
+                time: item.childNodes[2].childNodes[0].data
+            })),
+        date: dateString
     })
-)
+
 jsonBlob = new Blob([JSON.stringify(json)], {type: "application/json"})
 jsonURL = URL.createObjectURL(jsonBlob)
 
@@ -21,8 +28,7 @@ jsonURL = URL.createObjectURL(jsonBlob)
 newNode = document.createElement("a")
 newNode.className = "lbd-button white"
 newNode.href = jsonURL
-d = new Date()
-newNode.download = "crossword_" + [d.getFullYear(), d.getMonth() + 1, d.getDay()].join('-') + ".json"
+newNode.download = "crossword_" + dateString + ".json"
 newNode.appendChild(document.createTextNode("Extract Data"))
 
 // Append the new button
--- glicko.py
+++ glicko.py
@@ -4,7 +4,10 @@ import sys
 file = open(sys.argv[1])
 data = json.load(file)
 
-print([{
-        'name': item['name'],
-        'time': sum([a*b for a,b in zip([60,1], map(int, item['time'].split(':')))]) if item['time'] != '--' else -1
-    } for item in data])
+print({
+        'data': [{
+            'name': item['name'],
+            'time': sum([a*b for a,b in zip([60,1], map(int, item['time'].split(':')))])
+        } for item in data['data'] if item['time'] != '--'],
+        'date': data['date']
+    })
