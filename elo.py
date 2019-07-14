4f91d97f2059304dd99f404c6c595d9cb0b3dc7a
caldwellwg <caldwellwg@gmail.com>
2019-07-14 14:33:07 -0500

    Swapped list of dicts model to pandas dataframes

--- elo.py
+++ elo.py
@@ -1,6 +1,7 @@
 import json
 from scipy.special import gamma, gammaincc
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
@@ -12,10 +13,10 @@ K = 30
 def load_json(handle):
     data = json.load(handle)
     return {
-        'data': [{
+        'data': pd.DataFrame([{
             'name': item['name'],
             'time': sum([a*b for a, b in zip([60, 1], map(int, item['time'].split(':')))])
-        } for item in data['data'] if item['time'] != '--'],
+        } for item in data['data'] if item['time'] != '--']),
         'date': data['date']
     }
 
@@ -28,33 +29,29 @@ def score_gamma(x, k, theta):
     return gammaincc(k, x/theta)
 
 
-def update_ratings(new_json, players=[]):
+def update_ratings(new_json, players=pd.DataFrame(columns=['name', 'rating'])):
     # Fit to gamma distribution
-    times = [item['time'] for item in new_json['data']]
-    avg = np.average(times)
-    std = np.std(times)
+    data = new_json['data']
+    avg = data['time'].mean()
+    std = data['time'].std()
     k = (avg/std)**2
     theta = std**2/avg
 
-    for result in new_json['data']:
-        if not any(player['name'] == result['name'] for player in players):
-            players.append({
+    for _, result in data.iterrows():
+        if not (players['name'] == result['name']).any():
+            players = players.append({
                 'name': result['name'],
                 'rating': 1200
-            })
+            }, ignore_index=True)
 
-    average_rating = np.average([player['rating'] for player in players if player['name'] in map(
-        lambda i: i['name'], new_json['data'])])
-    for result in new_json['data']:
-        rating = next(player['rating']
-                      for player in players if player['name'] == result['name'])
+    average_rating = players[players['name'].isin(data['name'])]['rating'].mean()
+    for _, result in data.iterrows():
+        rating = players[players['name'] == result['name']]['rating']
         rating_diff = (rating - average_rating) / n
         expected_score = 1 / (1 + 0.1**rating_diff)
         actual_score = score_gamma(result['time'], k, theta)
         new_rating = rating + K * (actual_score - expected_score)
-        for player in players:
-            if player['name'] == result['name']:
-                player['rating'] = new_rating
+        players.update({'name': result['name'], 'rating': new_rating})
     return players
 
 
@@ -70,9 +67,9 @@ def plot_results(new_json):
     plt.show()
 
 
-players = []
+players = pd.DataFrame(columns=['name', 'rating'])
 for filename in sys.argv[1:]:
     with open(filename) as file:
         new_json = load_json(file)
         players = update_ratings(new_json, players)
-        print(sorted(players, key=lambda i: -i['rating']))
+        print(players.sort_values(by=['rating']))
