02dddf584a3e55140475657d17759b348ef57a54
caldwellwg <caldwellwg@gmail.com>
2019-07-24 14:25:24 -0500

    Added whitelisting and blacklisting.

--- elo.py
+++ elo.py
@@ -1,6 +1,6 @@
 import json
 import datetime as dt
-from scipy.special import gamma, gammaincc
+from scipy.special import gammaincc
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -10,6 +10,10 @@ import sys
 # Elo constants
 n = 400
 K = 10
+
+wblist = json.load(open('whiteblacklist.json'))
+whitelist = wblist['whitelist']
+blacklist = wblist['blacklist']
 
 
 def load_json(handle):
@@ -21,10 +25,6 @@ def load_json(handle):
         } for item in data['data'] if item['time'] != '--'],
         'date': data['date']
     }
-
-
-def gamma_dist(x, k, theta):
-    return x**(k - 1) * np.exp(-x / theta) / (gamma(k) * theta**k)
 
 
 def score_gamma(x, k, theta):
@@ -43,11 +43,11 @@ def forward_fill(arr):
     return ret
 
 
-def process_times():
-    c.execute('SELECT DISTINCT date FROM times')
-    for date in c.fetchall():
-        c.execute('SELECT name, time FROM times WHERE date=?', date)
-        today_data = dict(c.fetchall())
+def process_times(cur):
+    cur.execute('SELECT DISTINCT date FROM times')
+    for date in cur.fetchall():
+        cur.execute('SELECT name, time FROM times WHERE date=?', date)
+        today_data = dict(cur.fetchall())
         players = list(today_data.keys())
         times = list(today_data.values())
         avg = np.mean(times)
@@ -56,12 +56,13 @@ def process_times():
         k = (3 - s + np.sqrt((s - 3)**2 + 24 * s)) / (12 * s)
         theta = avg / k
 
-        c.execute('''SELECT name, rating
-                     FROM ratings
-                     WHERE name IN ({0})
-                     GROUP BY name
-                     ORDER BY date DESC'''.format(', '.join('?' for _ in players)), players)
-        player_data = dict(c.fetchall())
+        cur.execute('''SELECT name, rating
+                       FROM ratings
+                       WHERE name IN ({0})
+                       AND name NOT IN ({1})
+                       GROUP BY name
+                       ORDER BY date DESC'''.format(', '.join('?' for _ in players), ', '.join('?' for _ in blacklist)), players + blacklist)
+        player_data = dict(cur.fetchall())
         for player in players:
             if player not in player_data.keys():
                 player_data.update({player: 1200})
@@ -73,21 +74,8 @@ def process_times():
             expected_score = 1 / (1 + 10 ** rating_diff)
             actual_score = score_gamma(time, k, theta)
             new_rating = rating + K * (actual_score - expected_score)
-            c.execute('INSERT INTO ratings VALUES (?, ?, ?)',
-                      date + (name, new_rating))
-
-
-def plot_results(new_json):
-    data = new_json['data']
-    avg = data['time'].mean()
-    avglog = data['time'].apply(np.log).mean()
-    s = np.log(avg) - avglog
-    k = (3 - s + np.sqrt((s - 3) ** 2 + 24 * s)) / (12 * s)
-    theta = avg / k
-    x = np.linspace(0, 300)
-    plt.plot(x, gamma_dist(x, k, theta), c='blue')
-    plt.scatter(data['time'], np.zeros(data['time'].count()), c='red')
-    plt.show()
+            cur.execute('INSERT INTO ratings VALUES (?, ?, ?)',
+                        date + (name, new_rating))
 
 
 # Setup sqlite
@@ -96,7 +84,7 @@ c = conn.cursor()
 c.execute('''CREATE TABLE times
              (date text, name text, time int)''')
 c.execute('''CREATE TABLE ratings
-             (date text, name text, rating int)''')
+             (date text, name text, rating real)''')
 
 for filename in sys.argv[1:]:
     with open(filename) as h:
@@ -105,7 +93,7 @@ for filename in sys.argv[1:]:
                    for d in new_json['data']]
         c.executemany('INSERT INTO times VALUES (?,?,?)', results)
 
-process_times()
+process_times(c)
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator())
@@ -115,11 +103,12 @@ start, stop = map(np.datetime64, c.fetchone())
 dates = [date.astype(dt.datetime) for date in np.linspace(
     start.astype('f8'), stop.astype('f8'), dtype='<M8[D]')]
 
-c.execute('SELECT DISTINCT name FROM ratings')
+c.execute('''SELECT DISTINCT name FROM ratings
+             WHERE name IN ({0})'''.format(', '.join('?' for _ in whitelist)), whitelist)
 for player in c.fetchall():
     c.execute('SELECT date, rating FROM ratings WHERE name=?', player)
-    z = dict([(dt.datetime.strptime(date, '%Y-%m-%d').date(), rating)
-              for date, rating in c.fetchall()])
+    z = {dt.datetime.strptime(date, '%Y-%m-%d').date(): rating
+         for date, rating in c.fetchall()}
     plt.plot(dates, forward_fill([z.get(date)
                                   for date in dates]), label=player[0])
 
